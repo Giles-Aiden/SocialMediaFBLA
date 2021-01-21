89f02b5bf1b3ced21a7df8cde28bbe7941d29425
KillerHawk-Mec <agiles257@west-mec.org>
2021-01-21 15:00:28 -0700

    Infinate Scroll

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -5,24 +5,55 @@
         <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 5</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Home" />
+    <ion-content>
+
+      <ion-list>
+        <ion-item v-for='item in items' :key='item'>
+          <p>{{ item }}</p>
+        </ion-item>
+      </ion-list>
+
+      <ion-infinite-scroll
+      @ionInfinite="loadData($event)" 
+      threshold="10%" 
+      id="infinite-scroll">
+        <ion-infinite-scroll-content
+          loading-spinner="bubbles"
+          loading-text="Loading more data...">
+        </ion-infinite-scroll-content>
+      </ion-infinite-scroll>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
-export default  {
+export default {
   name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  data() {
+    return {
+      items: [1,2,3,4,5,6,7,8,9,10],
+      itemNum: 11,
+    }
+  },
+  methods: {
+    loadData(event) {
+      setTimeout(() => {
+        console.log('requesting more items');
+        this.addMoreItems();
+        event.target.complete();
+      }, 500);
+    },
+    addMoreItems() {
+      let itt = this.itemNum;
+      for (let i=itt; i<itt+10; i++) {
+        this.items.push(i);
+        this.itemNum++;
+      }
+    },
+  },
 }
 </script>
