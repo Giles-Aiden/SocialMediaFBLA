070d865ced3eda9d86e16627032e8c1ea7b31743
Aiden <agiles257@west-mec.org>
2021-02-05 18:18:55 -0700

    fakePeople

--- app/src/components/Post.vue
+++ app/src/components/Post.vue
@@ -1,34 +1,69 @@
 <template>
   <ion-card>
-    <ion-card-header>
-      <ion-card-subtitle>Posted by: {{ uinfo }}</ion-card-subtitle>
-      <ion-card-title>{{ title }}</ion-card-title>
-    </ion-card-header>
-    <ion-card-content>
-      {{ body }}
-      <ion-img :src="imgUrl" />
-    </ion-card-content>
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ion-card-header>
+            <ion-card-subtitle>Posted by: {{ uinfo }}</ion-card-subtitle>
+            <ion-card-title>{{ title }}</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            {{ body }}
+          </ion-card-content>
+        </ion-col>
+        <ion-col>
+          <div class="imgContain">
+            <ion-img :src="imgUrl" />
+          </div>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
   </ion-card>
 </template>
 
 <script>
-  
-  export default {
-    name: 'UserPost',
-    props: {
-      uinfo: String,
-      title: String,
-      body: String,
-      imgUrl: String
-    }
-  }
-
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/vue";
+export default {
+  name: "UserPost",
+  components: {
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonImg,
+    IonGrid,
+    IonRow,
+    IonCol,
+  },
+  props: {
+    uinfo: String,
+    title: String,
+    body: String,
+    imgUrl: String,
+  },
+  setup() {
+    imgUrl = "../fakePeople/man2.jpg";
+  },
+  methods: {},
+};
 </script>
 
 <style>
-  ion-img {
-    width: 5rem;
-    height: auto;
-    float: right;
-  }
+ion-img {
+  width: 5rem;
+  height: auto;
+  float: right;
+}
+.imgContain {
+  width: 100%;
+  align-content: center;
+  justify-content: center;
+}
 </style>
