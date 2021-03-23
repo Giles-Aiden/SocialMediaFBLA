8c904c1761fbcdd551dc664371b01eaaa9abcded
-g <-g>
2021-03-23 12:46:15 -0700

    Fix build.sh error

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 cd ./app
 npm run build
 cd dist
-cp -r ./ ../../docker/builds/app/
-cd ../../docker/builds
+cp -r ./ ../../docker/builds/app/dist
+cd ../../docker/builds/app
+npm install
 docker build -t cac-app .
-cd ../../server/
+cd ../../../server/
 docker build -t cac-server .
